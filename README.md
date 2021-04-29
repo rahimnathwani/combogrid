84cc152f97d3d5d4e555cf69a9f3dde329df2795
Rahim Nathwani <rahim@encona.com>
2021-04-28 19:10:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# combogrid
+
+**combogrid** makes it easier to draw combo charts in a grid, using matplotlib
+
+Combo charts are useful for comparing two different 'y' variables.
+Grids of charts (aka 'facet grids') are useful for comparing data
+between groups.
+
+Perhaps you want to see how a single stock's price and volume changed day by day.
+And you are following multiple stocks, so you want one chart per stock.
+
+```python
+>>> import pandas as pd
+>>> import combogrid
+>>> df = pd.read_csv("sample.csv")
+>>> df["date"] = pd.to_datetime(df["date"])
+>>> plt = combogrid.plot(
+>>>     df, "date", "volume", "price", "color"
+>>> )
+>>> plt.show()
+
+```
---
+++ combogrid/plot.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import datetime
+
+
+def plot(
+    df,
+    x,
+    y_line,
+    y_bar,
+    facet_dimension,
+    percent_cols=[],
+    style="fivethirtyeight",
+):
+    """Plots a grid of combo charts from a pandas dataframe.
+
+    Parameters
+    ----------
+    df : pandas.core.frame.DataFrame
+        The data you want to plot
+    x : str
+        The name of the column to plot on the horizontal axis
+    y_line : str
+        The name of the column to plot as a line
+    y_bar : str
+        The name of the column to plot as bars
+    facet_dimension : str
+        The name of the column to split the data into multiple charts
+    percent_cols : list of str, optional
+        The name(s) of the column(s) whose scale should be X% not 0.X (default is [])
+    style : str, optional
+        The matplotlib style (theme) to use (default is "fivethirtyeight")
+
+    Example
+    ------
+    NotImplementedError
+        If no sound is set for the animal or passed in as a
+        parameter.
+    """
+    y1, y2 = y_line, y_bar
+    plt.style.use(style)
+
+    @matplotlib.ticker.FuncFormatter
+    def decimal_to_percentage(x, pos):
+        return "{0:.0%}".format(x)
+
+    facets = set(df[facet_dimension])
+    ncols = 2
+    print(ncols)
+    nrows = int(np.ceil(len(facets) / ncols))
+    print(nrows)
+    indices = [(row, col) for row in range(nrows) for col in range(ncols)]
+    mapping = {facet: indices.pop() for facet in facets}
+    plt.rcParams.update({"figure.autolayout": True})
+    fig, ax = plt.subplots(nrows, ncols, figsize=(6 * ncols, 5 * nrows))
+    for facet in facets:
+        row, col = mapping[facet]
+        dff = df[df[facet_dimension] == facet]
+        ax1 = ax[row][col]
+        ax1.plot(dff[x], dff[y1], color="red")
+        if y1 in percent_cols:
+            ax1.yaxis.set_major_formatter(mtick.PercentFormatter)
+        ax1.set(
+            xlim=(df[x].min(), df[x].max()),
+            ylim=(df[y1].min(), df[y1].max()),
+            title=facet,
+        )
+        labels = ax1.get_xticklabels()
+        plt.setp(labels, rotation=45, horizontalalignment="right")
+        ax2 = ax1.twinx()
+        ax2.bar(dff[x], dff[y2], alpha=0.4)
+        if y2 in percent_cols:
+            ax2.yaxis.set_major_formatter(mtick.PercentFormatter())
+        ax2.set(xlim=(df[x].min(), df[x].max()), ylim=(df[y2].min(), df[y2].max()))
+    return plt
